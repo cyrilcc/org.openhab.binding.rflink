36bb8ad09f28bf037cc081b4ecb0e0130e1f5cf3
cartemere <cartemere@online.fr>
2019-01-25 10:08:59 +0100

    [SWITCH] #42: NewKaku ALLON/ALLOFF command support - fix declaration

--- src/main/java/org/openhab/binding/rflink/type/AllOnOffType.java
+++ src/main/java/org/openhab/binding/rflink/type/AllOnOffType.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.rflink.type;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -5,6 +13,11 @@ import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.PrimitiveType;
 import org.eclipse.smarthome.core.types.State;
 
+/**
+ * Provides Type for NewKaku Switch : ALLON, ALLOFF
+ * 
+ * @author cartemere
+ */
 @NonNullByDefault
 public enum AllOnOffType implements PrimitiveType, State, Command {
     ALLON,
