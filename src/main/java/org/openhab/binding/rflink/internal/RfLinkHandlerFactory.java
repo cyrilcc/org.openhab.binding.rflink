2e376da9c2ad41168a825eaa9fb7dc94bacf10b0
Petr Shatsillo <Pshatsillo@gmail.com>
2020-06-16 20:28:14 +0300

    Refactoring for OPENHAB 2.5.6 building system

--- src/main/java/org/openhab/binding/rflink/internal/RfLinkHandlerFactory.java
+++ src/main/java/org/openhab/binding/rflink/internal/RfLinkHandlerFactory.java
@@ -12,6 +12,8 @@
  */
 package org.openhab.binding.rflink.internal;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.config.discovery.DiscoveryService;
 import org.eclipse.smarthome.core.thing.Bridge;
 import org.eclipse.smarthome.core.thing.Thing;
@@ -19,11 +21,13 @@ import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.ThingUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
 import org.openhab.binding.rflink.RfLinkBindingConstants;
 import org.openhab.binding.rflink.handler.RfLinkBridgeHandler;
 import org.openhab.binding.rflink.handler.RfLinkHandler;
 import org.openhab.binding.rflink.internal.discovery.RfLinkDeviceDiscoveryService;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,6 +44,8 @@ import java.util.stream.Stream;
  *
  * @author Cyril Cauchois - Initial contribution
  */
+@NonNullByDefault
+@Component(configurationPid = "binding.rflink", service = ThingHandlerFactory.class)
 public class RfLinkHandlerFactory extends BaseThingHandlerFactory {
     private Logger logger = LoggerFactory.getLogger(RfLinkHandlerFactory.class);
 
@@ -58,7 +64,7 @@ public class RfLinkHandlerFactory extends BaseThingHandlerFactory {
     }
 
     @Override
-    protected ThingHandler createHandler(Thing thing) {
+    protected @Nullable ThingHandler createHandler(Thing thing) {
 
         logger.debug("RfLinkHandlerFactory createHandler({})", thing.getUID().toString());
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
